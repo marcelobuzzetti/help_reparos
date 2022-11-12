a3c3892d6da606ca41e28b55359421c0794a2226
Marcelo Buzzeti <marcelobuzzetti@gmail.com>
2022-11-12 09:11:14 -0300

    model and resource

--- app/Models/os.php
+++ app/Models/os.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class os extends Model
+class Os extends Model
 {
     use HasFactory;
 }

0801f1650bcff26ee6e0300c9da58cc4875f054a
Marcelo Buzzeti <marcelobuzzetti@gmail.com>
2022-11-16 09:34:06 -0300

    css

--- resources/views/ordem/create.blade.php
+++ resources/views/ordem/create.blade.php
@@ -2,6 +2,7 @@
 
 @section('content')
     <div class="card-glass">
+        <div class="container">
         <div class="row">
             <div class="col-lg-12 margin-tb d-flex justify-content-center">
                 <div class="pull-left">
@@ -14,7 +15,7 @@
             @csrf
 
             <div class="row">
-                <div class="col-xs-12 col-sm-12 col-md-12 mb-2">
+                <div class="col-xs-12 col-sm-12 col-md-6 mb-2">
                     <div class="form-group">
                         <strong>Cliente</strong>
                         <select name="cliente_id" class="form-control @error('cliente_id') is-invalid @enderror">
@@ -34,7 +35,7 @@
                         @enderror
                     </div>
                 </div>
-                <div class="col-xs-12 col-sm-12 col-md-12 mb-2">
+                <div class="col-xs-12 col-sm-12 col-md-6 mb-2">
                     <div class="form-group">
                         <strong>Marca</strong>
                         <select name="marca_id" class="form-control @error('marca_id') is-invalid @enderror">
@@ -54,7 +55,7 @@
                         @enderror
                     </div>
                 </div>
-                <div class="col-xs-12 col-sm-12 col-md-12 mb-2">
+                <div class="col-xs-12 col-sm-12 col-md-4 mb-2">
                     <div class="form-group">
                         <strong>Tipo de Aparelho</strong>
                         <input type="text" name="tipo_aparelho" class="form-control @error('tipo_aparelho') is-invalid @enderror" placeholder="Tipo de Aparelho" value="{{ old('tipo_aparelho') }}">
@@ -63,7 +64,7 @@
                         @enderror
                     </div>
                 </div>
-                <div class="col-xs-12 col-sm-12 col-md-12 mb-2">
+                <div class="col-xs-12 col-sm-12 col-md-4 mb-2">
                     <div class="form-group">
                         <strong>Modelo</strong>
                         <input type="text" name="modelo" class="form-control @error('modelo') is-invalid @enderror" placeholder="Modelo" value="{{ old('modelo') }}">
@@ -72,7 +73,7 @@
                         @enderror
                     </div>
                 </div>
-                <div class="col-xs-12 col-sm-12 col-md-12 mb-2">
+                <div class="col-xs-12 col-sm-12 col-md-4 mb-2">
                     <div class="form-group">
                         <strong>Estado do Aparelho</strong>
                         <input type="text" name="estado_aparelho" class="form-control @error('estado_aparelho') is-invalid @enderror" placeholder="Estado do Aparelho" value="{{ old('estado_aparelho') }}">
@@ -81,7 +82,7 @@
                         @enderror
                     </div>
                 </div>
-                <div class="col-xs-12 col-sm-12 col-md-12 mb-2">
+                {{-- <div class="col-xs-12 col-sm-12 col-md-12 mb-2">
                     <div class="form-group">
                         <strong>Defeito Alegado</strong>
                         <input type="text" name="defeito_alegado" class="form-control @error('defeito_alegado') is-invalid @enderror" placeholder="Defeito Alegado" value="{{ old('defeito_alegado') }}">
@@ -89,8 +90,17 @@
                             <div class="invalid-feedback">{{ $message }}</div>
                         @enderror
                     </div>
-                </div>
+                </div> --}}
                 <div class="col-xs-12 col-sm-12 col-md-12 mb-2">
+                    <div class="form-group">
+                        <strong>Defeito Alegado</strong>
+                        <textarea name="defeito_alegado" class="form-control @error('defeito_alegado') is-invalid @enderror">{{ old('defeito_alegado') }}</textarea>
+                        @error('defeito_alegado')
+                            <div class="invalid-feedback">{{ $message }}</div>
+                        @enderror
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-12 col-md-6 mb-2">
                     <div class="form-group">
                         <strong>Valor do Serviço</strong>
                         <input type="number" step="any" min="0.01" name="valor_servico" class="form-control @error('valor_servico') is-invalid @enderror" placeholder="Valor Servico" value="{{ old('valor_servico') }}">
@@ -99,7 +109,7 @@
                         @enderror
                     </div>
                 </div>
-                <div class="col-xs-12 col-sm-12 col-md-12 mb-2">
+                <div class="col-xs-12 col-sm-12 col-md-6 mb-2">
                     <div class="form-group">
                         <strong>Status</strong>
                         <select name="status_id" class="form-control @error('status_id') is-invalid @enderror">
@@ -141,6 +151,7 @@
             </div>
         </div>
     </div>
+</div>
     <script>
          $(document).ready(function() {
             $("[name='cliente_id']").select2();

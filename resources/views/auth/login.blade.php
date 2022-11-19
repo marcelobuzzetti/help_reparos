661b3f949f866d41f21a523d6262886ba4a76152
Marcelo Buzzeti <marcelobuzzetti@gmail.com>
2022-11-19 08:13:32 -0300

    login error

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -19,7 +19,7 @@
 
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
+                                        <strong>Dados inválidos</strong>
                                     </span>
                                 @enderror
                             </div>

e51fc7f7fc267349a29622a0acb36d8ff541f673
Marcelo Buzzeti <marcelobuzzetti@gmail.com>
2022-12-16 11:46:12 -0300

    show garantia

--- resources/views/empresa/show.blade.php
+++ resources/views/empresa/show.blade.php
@@ -51,7 +51,7 @@
             <div class="col-xs-12 col-sm-12 col-md-12">
                 <div class="form-group">
                     <strong>Garantia:</strong>
-                    {{ $empresa->garantia }} Dias
+                    {{ $empresa->garantia ? $empresa->garantia . " Dias" : "Não lançado" }}
                 </div>
             </div>
             <div class="col-xs-12 col-sm-12 col-md-12">
